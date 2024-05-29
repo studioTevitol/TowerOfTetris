1e10c113fd59bd4dea0d3f686f5fc5f79f01dbbf
DESKTOPN0-D3V\Nodev <kzycaliskan@gmail.com>
2024-05-29 11:59:27 +0300

    Added a charged jump mechanic and UI for charged jump.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,90 +1,154 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    Tetromino attachedTetromino;
-    Rigidbody2D Prigidbody;
-    Block currentBlock = null;
-    GameObject game;
-    public float horizontalSpeed = 3f;
-    public float jumpForce = 5f;
-    public bool isGrounded = false;
+    [Header("References")]
+    [SerializeField] private Image radialBar;
     
-    void Start()
+    [Header("Movement Settings")]
+    [SerializeField] private float horizontalSpeed = 3f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float maxChargeTime = 0f;
+    
+    private Tetromino attachedTetromino;
+    private Rigidbody2D Prigidbody;
+    private Block currentBlock = null;
+    private GameObject game;
+    private bool isGrounded = false;
+
+    // Initialize Player
+    private void Start()
     {
         attachedTetromino = null;
         Prigidbody = GetComponent<Rigidbody2D>();
         Prigidbody.isKinematic = true;
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //hold space to jump, press space to release. charge up jump by holding space
-        if (Input.GetKeyDown(KeyCode.Space)&&isGrounded)
+        // Hold space to jump, press space to release. Charge up jump by holding space
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            currentBlock = null;
-            attachedTetromino = null;
-            isGrounded = false;
-            Prigidbody.isKinematic = false;
-            Prigidbody.AddForce(new Vector2(0, jumpForce));
+            StartCoroutine(ChargeJump());
         }
-        if (!Prigidbody.isKinematic) Prigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed, Prigidbody.velocity.y);
+        
+        if (!Prigidbody.isKinematic)
+        {
+            Prigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed, Prigidbody.velocity.y);
+        }
 
-        if (Input.GetKeyDown(KeyCode.W)) Move(new Vector2(0, -1)); 
-        else if (Input.GetKeyDown(KeyCode.S)) Move(new Vector2(0, 1));
-        else if (Input.GetKeyDown(KeyCode.A)) Move(new Vector2(-1, 0));
-        else if (Input.GetKeyDown(KeyCode.D)) Move(new Vector2(1, 0));    
+        // Move character in tetromino with WASD
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Move(new Vector2(0, -1));
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(new Vector2(0, 1));
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(new Vector2(-1, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(new Vector2(1, 0));
+        }
+
+        if (currentBlock == null)
+        {
+            isGrounded = false;
+        }
+
+        // Update UI
+        radialBar.GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0.5f, 0.5f, 0));
     }
 
-
-    //collisiona girince ba�l� olunan tetrominoyu ayarla
+    // Set attachedTetromino and currentBlock to the block that the player is standing on
     private void OnTriggerEnter2D(Collider2D collision)
     {
         currentBlock = collision.GetComponent<Block>();
-        if (currentBlock!= null) {
-        isGrounded = true;
-        transform.localPosition = new Vector3(currentBlock.transform.position.x, currentBlock.transform.position.y, 1);
-        attachedTetromino = currentBlock.GetComponentInParent<Tetromino>();
-        Prigidbody.isKinematic = true;
-        Prigidbody.velocity = Vector2.zero;
-        attachedTetromino.SetPlayer(this);
+        
+        if (currentBlock != null)
+        {
+            isGrounded = true;
+            transform.localPosition = new Vector3(currentBlock.transform.position.x, currentBlock.transform.position.y, 1);
+            attachedTetromino = currentBlock.GetComponentInParent<Tetromino>();
+            Prigidbody.isKinematic = true;
+            Prigidbody.velocity = Vector2.zero;
+            attachedTetromino.SetPlayer(this);
         }
     }
-    void Move(Vector2 direction)
+
+    private void Move(Vector2 direction)
     {
-        Debug.Log(direction.x + ", " + direction.y);
         int currentBlockIndexX = -1;
         int currentBlockIndexY = -1;
+
         if (attachedTetromino != null)
         {
             for (int i = 0; i < attachedTetromino.structure.Count; i++)
             {
-                if (attachedTetromino.structure[i] == null || !attachedTetromino.structure[i].Contains(currentBlock)) continue;
+                if (attachedTetromino.structure[i] == null || !attachedTetromino.structure[i].Contains(currentBlock))
+                {
+                    continue;
+                }
+
                 currentBlockIndexX = attachedTetromino.structure[i].IndexOf(currentBlock);
                 currentBlockIndexY = i;
-                Debug.Log("index: " + currentBlockIndexX + ", rank: " + currentBlockIndexY);
-                Debug.Log("Findex: " + (int)(currentBlockIndexX + direction.x) + ", Frank: " + (int)(currentBlockIndexY + direction.y));
                 break;
             }
-            if (currentBlockIndexX != -1 && currentBlockIndexY != -1)
+
+            if (currentBlockIndexX != -1 && currentBlockIndexY != -1 && attachedTetromino.structure[(int)(currentBlockIndexY + direction.y)][(int)(currentBlockIndexX + direction.x)] != null)
             {
                 SetCurrentBlock(attachedTetromino.structure[(int)(currentBlockIndexY + direction.y)][(int)(currentBlockIndexX + direction.x)]);
                 transform.localPosition = new Vector3(currentBlock.transform.position.x, currentBlock.transform.position.y, 1);
             }
         }
-        direction = new Vector2(0, 0);
+
+        direction = Vector2.zero;
     }
+
+    private IEnumerator ChargeJump()
+    {
+        float chargeTime = 0;
+
+        while (Input.GetKey(KeyCode.Space) && chargeTime < maxChargeTime)
+        {
+            chargeTime += Time.deltaTime;
+            // Update charging bar UI
+            radialBar.fillAmount = 1 - chargeTime / maxChargeTime;
+            yield return null;
+        }
+
+        while (Input.GetKey(KeyCode.Space))
+        {
+            yield return null;
+        }
+
+        currentBlock = null;
+        attachedTetromino = null;
+        isGrounded = false;
+        Prigidbody.isKinematic = false;
+        float jumpForceMultiplier = chargeTime / maxChargeTime; // Adjust this formula to change the jump force based on charge time
+        Prigidbody.AddForce(new Vector2(0, jumpForce * jumpForceMultiplier));
+    }
+
     public void SetCurrentBlock(Block block)
     {
         currentBlock = block;
-        attachedTetromino = block.GetComponentInParent<Tetromino>();
+
+        if (block != null)
+        {
+            attachedTetromino = block.GetComponentInParent<Tetromino>();
+        }
+
         attachedTetromino.SetPlayer(this);
     }
+
     public Block GetCurrentBlock()
     {
         return currentBlock;
